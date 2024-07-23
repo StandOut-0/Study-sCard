7f09113c519b8d2c9477193c6a9eb75afc2f4ce3
StandOut-0 <standout.first@gmail.com>
2024-07-23 15:24:47 +0900

    new Spring Starter Project

---
+++ src/main/java/com/standout/scard/StudySCardApplication.java
@@ -0,0 +1,13 @@
+package com.standout.scard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySCardApplication.class, args);
+	}
+
+}
